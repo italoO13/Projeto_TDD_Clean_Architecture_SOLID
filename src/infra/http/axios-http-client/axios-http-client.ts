f35d439e83a6a31b63bd908d731e696dd1edba52
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-11-17 21:43:18 -0300

    feat: inserido AxiosHttpClient retornando os valores correto

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,8 +1,13 @@
-import { ParamnsRequestPost } from '@/data/usecases/protocols/http'
+import { HttpPostClient, HttpResponse, ParamnsRequestPost } from '@/data/usecases/protocols/http'
 import axios from 'axios'
 
-export class AxiosHttpClient {
-  async post (params: ParamnsRequestPost<any>): Promise<void> {
-    await axios.post(params.url, params.body)
+// Adapter HttpClient
+export class AxiosHttpClient implements HttpPostClient<any, HttpResponse<any>> {
+  async post (params: ParamnsRequestPost<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await axios.post(params.url, params.body)
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
   }
 }
