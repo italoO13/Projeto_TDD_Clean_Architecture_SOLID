14d350cf627aba228b22dbafe44462cd345c6eec
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-11-17 21:22:45 -0300

    refactor: movido SUT para uma função factory

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -5,10 +5,14 @@ import { faker } from '@faker-js/faker'
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
+const makeSut = (): AxiosHttpClient => {
+  return new AxiosHttpClient()
+}
+
 describe('AxiosHttpClient', () => {
   test('Verifica o axios é chamado com a URL correta', async () => {
     const url = faker.internet.url()
-    const sut = new AxiosHttpClient()
+    const sut = makeSut()
     await sut.post({ url })
     expect(mockedAxios).toBeCalledWith(url)
   })
