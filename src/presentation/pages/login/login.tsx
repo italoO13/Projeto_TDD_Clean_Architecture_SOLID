9794e23239c6cc7b1600c6db72fed2a7044be094
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-11-19 14:43:52 -0300

    refactor: add index nas pastas de page e components para multiplos imports

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import Styles from './login-styles.scss'
-import Spinner from '../../components/spinner/spinner'
-import Header from '../../components/login-header/login-header'
-import Footer from '@/presentation/components/footer/footer'
-import Input from '@/presentation/components/input/input'
-import FormStatus from '@/presentation/components/form-status/form-status'
+import { Input, FormStatus, Footer, LoginHeader } from '@/presentation/components/'
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <Header />
+      <LoginHeader />
       <form className={Styles.form}>
         <h2>Login</h2>
         <Input type="email" placeholder="Digite seu e-mail" name="email" />
