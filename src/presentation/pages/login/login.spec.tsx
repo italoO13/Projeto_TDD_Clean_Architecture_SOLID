528e580c70489c48a54f4889c4887a4058996c96
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-11-20 22:14:29 -0300

    test: garante que login não será renderizado com spinner e mensagem de error ao iniciar

---
+++ src/presentation/pages/login/login.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Login from './login'
+
+describe('Componente Login', () => {
+  test('Verifica se inicialmente a mensagem de error e o spinner não são renderizados na tela', () => {
+    const { getByTestId } = render(<Login />)
+    const errorWrap = getByTestId('error-wrap')
+    expect(errorWrap.childElementCount).toBe(0)
+  })
+})
