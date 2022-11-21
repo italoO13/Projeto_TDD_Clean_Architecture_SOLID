4861edd166b50f0889354cd39cab01a779959332
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-11-20 22:23:04 -0300

    test: garante que ao iniciar login o botão de submissão esteja desabilitado

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -8,4 +8,9 @@ describe('Componente Login', () => {
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
   })
+  test('Verifica se botão de submit inicialmente está desabilitado', () => {
+    const { getByTestId } = render(<Login />)
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
+  })
 })
