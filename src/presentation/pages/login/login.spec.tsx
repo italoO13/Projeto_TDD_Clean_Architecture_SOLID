05847fd7b632f0387cfb45c1a310af6348dbbd60
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-12-04 16:08:07 -0300

    test: garante que login chama uma validação que garante o email correto

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,19 +1,39 @@
 import React from 'react'
-import { getByTestId, render, RenderResult } from '@testing-library/react'
+import { faker } from '@faker-js/faker'
+import { render, RenderResult, cleanup } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Login from './login'
+import { Validation } from '@/presentation/protocols/validation'
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
+}
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+
+  validate (input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
 }
 
 const makeSut = (): SutTypes => {
-  const sut = render(<Login />)
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Login validation = {validationSpy} />)
   return {
-    sut
+    sut,
+    validationSpy
   }
 }
 
 describe('Componente Login', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
   test('Verifica se inicialmente a mensagem de error e o spinner não são renderizados na tela', () => {
     const { sut } = makeSut()
     const errorWrap = sut.getByTestId('error-wrap')
@@ -32,5 +52,15 @@ describe('Componente Login', () => {
     const passwordStatus = sut.getByTestId('password-status')
     expect(passwordStatus.title).toBe('Campo obrigatório')
     expect(passwordStatus.textContent).toBe('🔴')
+  })
+
+  test('Verifica se validação é chamada com os valores corretos', async () => {
+    const { sut, validationSpy } = makeSut()
+    const emailInput = sut.getByTestId('email')
+    const emailMock = faker.internet.email()
+    await userEvent.type(emailInput, emailMock)
+    expect(validationSpy.input).toEqual({
+      email: emailMock
+    })
   })
 })
